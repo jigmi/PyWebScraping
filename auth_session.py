cdb967cfdd87db4207f46cb7579999c4cb09d068
jiggy <jigmeydorjee1@protonmail.com>
2023-08-17 16:43:46 +1000

    Cleaned code and added request to change pass

--- auth_session.py
+++ auth_session.py
@@ -7,18 +7,30 @@ def main():
     username = config.username
     password = config.password
     user_agent = {'User-agent': 'Mozilla/4.0'}
-    payload = {
+    payload_login = {
         "action":"login",
         "user":username,
         "pass":password,
         "token":None,
         "remember":0
     }
+    change_pass = {
+        "action":"changePassword",
+        "p1":'iluvsecurity',
+        "p2":'iluvsecurity',
+        "credlogout":1,
+        "token":None
+    }
     # http/https is stateless, therefore every request sent must be within session
     with requests.session() as session:
-        session.post("https://whirlpool.net.au/profile/ajax.cfm",data=payload)
+        # Authenticating session
+        session.post("https://whirlpool.net.au/profile/ajax.cfm",data=payload_login)
         source = session.get("https://whirlpool.net.au/profile/?action=account")
         source = BeautifulSoup(source.text,'html.parser')
+        print(source)
+        # Changing pass 
+        post_response = session.post("https://whirlpool.net.au/profile/ajax.cfm",data=change_pass)
+        post_response = BeautifulSoup(source.text,'html.parser')
         print(source)
 
 if __name__ == '__main__':
